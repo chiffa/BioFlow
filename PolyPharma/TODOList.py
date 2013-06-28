edb9d420169e6672b1a9f24b3bf31f12591e4c9e
chiffa <andrei.chiffa136@gmail.com>
2013-06-28 16:40:24 -0700

    Updated the global TODO list for the whole programming task

--- PolyPharma/TODOList.py
+++ PolyPharma/TODOList.py
@@ -4,6 +4,8 @@ Created on Jun 25, 2013
 @author: andrei
 '''
 
+# TODO: Unify DatabaseGraphDeclarations
+
 # TODO: Include mappings to the Gene onthology
 # TODO: Include mappings to the EC numbers from UNIPROT
 
@@ -13,4 +15,19 @@ Created on Jun 25, 2013
 # On parse, add an additional "is_possibly the same" if a reactome protein displayName is the same as a UNIPROT SWISSPROT name without the 
 
 # TODO: for each uniprot gene iserted add a DNA reference to the gene that was actually concerned.
-# That sucks, because we will also need to insert all the names references and check for the possible collisions with the UNIPROT identifiers 
+# That sucks, because we will also need to insert all the names references and check for the possible collisions with the UNIPROT identifiers
+
+# TODO: Matrix retriever:
+# 1 -> 2 -> 3
+#
+# for each new insertion, look if it modifies any direct pathway
+# even better: make all the insertions at 1 on the diagonal, then sup triangularise the matrix and fill in the same way as for the energy interaction: take the shortest interaction
+# points (dynamic programming)
+#
+# Wait, can we do it by matrix multiplication? 
+#
+# 0 1 ?     0 1 2
+#   0 1  ->   0 1
+#     0         0
+
+# We are not taking in account the EC numbers of enzymes, since they do not point towards the reaction participants, just the catalysing elements.
