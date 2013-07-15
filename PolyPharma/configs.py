22a7352232c4210a49565778ef703d3ee16849c6
chiffa <andrei.chiffa136@gmail.com>
2013-07-15 16:52:08 -0700

    Added exclusion lists to exclude the most popular molecules (ATP/ADP, etc...)

--- PolyPharma/configs.py
+++ PolyPharma/configs.py
@@ -17,3 +17,27 @@ ReactomeBioPax='/home/andrei/workspaces/UCSD/Parsing_Reactome/Homo sapiens.owl'
 UNIPROT_text='/home/andrei/workspaces/UCSD/uniprot_sprot.dat'
 Hint_csv='/home/andrei/workspaces/UCSD/sapiens_curated-interactome.csv' #attention, for me it is tab-separated
 
+IDFilter=['5379',     # Small_Molecule_Collection Purine nucleotide
+          '5298',     # Small_Molecule_Collection ADP, GDP, CDP, UDP
+          '816',      # Small_Molecule ATP
+          '2419',     # Small_Molecule ADP
+          '5391',     # Small_Molecule_Collection (d)ADP
+          '40020',    # Small_Molecule H2O
+          '2047',     # Small_Molecule H+
+          '5296',     # Small_Molecule_Collection ADP, GDP, CDP, UDP
+          '2795',     # ('Small_Molecule', u'H2O', 'cytosol')  (All the follwong retrieved by the eignevalue approach)
+          '2217',     # ('Small_Molecule', u'H2O', 'lysosomal lumen')
+          '806',      # ('Small_Molecule', u'Pi', 'cytosol')
+          '4055',     # ('Small_Molecule', u'H2O', 'extracellular region')
+          '2880',     # ('Small_Molecule', u'CO2', 'cytosol')
+          '2317',     # ('Small_Molecule', u'O2', 'cytosol')
+          '2803',     # ('Small_Molecule', u'PPi', 'cytosol')
+          '2045',     # ('Small_Molecule', u'H+', 'extracellular region')
+          '812',      # ('Small_Molecule', u'ATP', 'nucleoplasm')
+          '1758',     # ('Small_Molecule', u'ATP', 'mitochondrial matrix')
+          '1740',     # ('Small_Molecule', u'H+', 'mitochondrial matrix')
+          '992',      # ('Small_Molecule', u'H2O', 'endoplasmic reticulum lumen')
+          '2429',     # ('Small_Molecule', u'H2O', 'nucleoplasm')
+          '1240',     # ('Small_Molecule', u'O2', 'peroxisomal matrix')
+          '818',      # ('Small_Molecule', u'Pi', 'nucleoplasm')
+               ]
--- PolyPharma/neo4j_analyzer/Matrix_Puller.py
+++ PolyPharma/neo4j_analyzer/Matrix_Puller.py
@@ -4,6 +4,7 @@ Created on Jul 11, 2013
 @author: andrei
 '''
 from neo4j_Declarations.Graph_Declarator import DatabaseGraph
+from configs import IDFilter
 import copy
 from scipy.sparse import lil_matrix
 from scipy.sparse.linalg import eigsh
@@ -11,6 +12,7 @@ import itertools
 from time import time
 import pickle
 import numpy as np
+import operator
 
 edge_type_filter0_1=["is_part_of_collection"]
 edge_type_filter0_2=["is_same"]
@@ -48,9 +50,10 @@ def get_Reaction_blocks():
                     if Reaction.bothV(edge_type)!=None:
                         for elt in Reaction.bothV(edge_type):
                             ID=str(elt).split('/')[-1][:-1]
-                            LocalList.append(ID)
-                            Seeds.add(ID)
-                            count+=1
+                            if ID not in IDFilter:
+                                LocalList.append(ID)
+                                Seeds.add(ID)
+                                count+=1
                 ReagentClusters.append(copy.copy(LocalList))
     return ReagentClusters, Seeds, count
 
@@ -66,9 +69,10 @@ def get_expansion(SubSeed,edge_type_filter):
             if SeedNode.bothV(edge_type)!=None:
                 for elt in SeedNode.bothV(edge_type):
                     ID=str(elt).split('/')[-1][:-1]
-                    LocalList.append(ID)
-                    SuperSeed.add(ID)
-                    count+=1
+                    if ID not in IDFilter:
+                        LocalList.append(ID)
+                        SuperSeed.add(ID)
+                        count+=1
         if len(LocalList)>1:
             Clusters.append(copy.copy(LocalList))
     return Clusters, SuperSeed, count
@@ -78,25 +82,35 @@ def build_correspondances(IDSet):
     MatrixNumber2NodeID={}
     ID2displayName={}
     ID2Type={}
+    ID2Localization={}
     counter=0
+    LocationBufferDict={}
     for ID in IDSet:
         NodeID2MatrixNumber[ID]=counter
         MatrixNumber2NodeID[counter]=ID
         Vertex=DatabaseGraph.vertices.get(ID)
         ID2displayName[ID]=Vertex.displayName
         ID2Type[ID]=Vertex.element_type
+        if Vertex.localization!=None:
+            ID2Localization[ID]=request_location(LocationBufferDict,Vertex.localization)
         counter+=1
-    return NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type
+    return NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type, ID2Localization
+
+def request_location(LocationBufferDict,location):
+    location=str(location)
+    if location in LocationBufferDict.keys():
+        return LocationBufferDict[location]
+    else:
+        generator=DatabaseGraph.Location.index.lookup(ID=location)
+        if generator!=None:
+            for elt in generator:
+                LocationBufferDict[location]=str(elt.displayName)
+                return str(elt.displayName)
+
 
 def getMatrix(decreaseFactorDict):
-    NodeID2MatrixNumber={}
-    MatrixNumber2NodeID={}
-    ID2displayName={}
-    ID2Type={}
     init=time()
-    
-    # Connect the groups of ingredients that share the same reactions
-    
+    # Connect the groups of ingredients that share the same reactions1
     # Retrieve seeds for the matrix computation
     ReactLinks, InitSet, c = get_Reaction_blocks()
     print len(ReactLinks), len(InitSet), c
@@ -150,10 +164,12 @@ def getMatrix(decreaseFactorDict):
     # ACHTUNG!!!!! all the lanes have to be normalized later on to be effectively corresponding to a Markov model!!!! => Not in our case, since some proteins are affecting
     # SEVERAL other proteins at the same time
     
+    # Limitations: no physical-path toxicity (such as rising pH, changing the O2 content or depleting ATP/ADP)
+    
     print "building correspondances", time()-init, time()-t
     t=time()
     
-    NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type = build_correspondances(ExpSet)
+    NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type, ID2Localization = build_correspondances(ExpSet)
     
     
     print "building the ValMatrix", time()-init, time()-t
@@ -184,33 +200,67 @@ def getMatrix(decreaseFactorDict):
             element=(NodeID2MatrixNumber[elt[0]],NodeID2MatrixNumber[elt[1]])
             ValueMatrix[element[0],element[1]]=min(ValueMatrix[element[0],element[1]]+decreaseFactorDict["possibly_same"],1-1e3)        
     
-#     print "entering eigenvect computation", time()-init, time()-t
-#     t=time()
-#     eigenvals, eigenvects = eigsh(ValueMatrix,1000)
-#     print eigenvals
-#     output = file('eigenvals.csv','w')
-#     output.write(eigenvals)
-#     print eigenvects.shape
-#     pickleDump=file('pickleDump','w')
-#     pickle.dump(eigenvects, pickleDump)
-#     pickle.dump.close()
-    pickleDump2=file('pickleDump2','w')
-    pickle.dump((NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type),pickleDump2)
+    print "entering eigenvect computation", time()-init, time()-t
+    t=time()
+    eigenvals, eigenvects = eigsh(ValueMatrix,1000)
+    print eigenvals
+    output = file('eigenvals.csv','w')
+    output.write(eigenvals)
+    pickleDump=file('pickleDump.dump','w')
+    pickle.dump(eigenvects, pickleDump)
+    pickleDump.close()
+    pickleDump2=file('pickleDump2.dump','w')
+    pickle.dump((NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type, ID2Localization),pickleDump2)
     pickleDump2.close()
-    pickleDump3=file('pickleDump3','w')
+    pickleDump3=file('pickleDump3.dump','w')
     pickle.dump(ValueMatrix,pickleDump3)
     pickleDump3.close()
     
     print time()-init, time()-t
-    # find out the configuration most important elements for the eigenvectors, then get back to their name
+
+def get_eigenvect_Stats():
+    init=time()
+    pickleDump=file('pickleDump.dump','r')
+    eigenvects=pickle.load(pickleDump)
+    pickleDump2=file('pickleDump2.dump','r')
+    NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type, ID2Localization = pickle.load(pickleDump2)
+    pickleDump3=file('pickleDump3.dump','r')
+    ValueMatrix=pickle.load(pickleDump3)
+    eigenVectsList=[]
+    SuperIndex={}
+    CounterIndex={}
+    print 'depicked',time()-init
+    for i in range(0,eigenvects.shape[1]):
+        eigenVectsList.append(eigenvects[:,i])
+    i=0
+    for eigenvect in eigenVectsList:
+        i+=1
+        normalized=np.multiply(eigenvect,eigenvect)
+        for k in range(0,10):
+            index=np.argmax(normalized, 0)
+            if MatrixNumber2NodeID[index] not in CounterIndex.keys():
+                CounterIndex[MatrixNumber2NodeID[index]]=0
+                if MatrixNumber2NodeID[index] in ID2Localization.keys():
+                    SuperIndex[MatrixNumber2NodeID[index]]=(ID2Type[MatrixNumber2NodeID[index]], ID2displayName[MatrixNumber2NodeID[index]], ID2Localization[MatrixNumber2NodeID[index]])
+                else: 
+                    SuperIndex[MatrixNumber2NodeID[index]]=(ID2Type[MatrixNumber2NodeID[index]], ID2displayName[MatrixNumber2NodeID[index]])
+            CounterIndex[MatrixNumber2NodeID[index]]+=normalized[index]
+            normalized[index]=0
+    srtd=sorted(CounterIndex.iteritems(), key=operator.itemgetter(1),reverse=True)
+    print IDFilter
+    for key,val in srtd[:100]:
+        print val, key, SuperIndex[key], key in IDFilter
+        if key in IDFilter:
+            print 'error on key: ', key
+    return CounterIndex, SuperIndex
     
 def processEigenVectors():
     init=time()
-    pickleDump=file('pickleDump','r')
+    pickleDump=file('pickleDump.dump','r')
     eigenvects=pickle.load(pickleDump)
-    pickleDump2=file('pickleDump2','r')
-    NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type = pickle.load(pickleDump2)
-    pickleDump3=file('pickleDump3','r')
+    pickleDump2=file('pickleDump2.dump','r')
+    NodeID2MatrixNumber, MatrixNumber2NodeID, ID2displayName, ID2Type, ID2Localization = pickle.load(pickleDump2)
+    pickleDump3=file('pickleDump3.dump','r')
     ValueMatrix=pickle.load(pickleDump3)
     eigenVectsList=[]
     SuperIndex=[]
@@ -236,11 +286,11 @@ def processEigenVectors():
         print '<=========================>'
     return SuperIndex
 
-# getMatrix(DfactorDict)
+getMatrix(DfactorDict)
 
-processEigenVectors()
+# processEigenVectors()
 
-# TODO: create compound exclusion list
+get_eigenvect_Stats()
 
 # TODO: create GO and Pathway Structure access
-# TODO: retrieve the most connected nodes and disconnect them (ATP, H2O, etc..)
+# Calibrate the values so that after ~3 transitions the correlation vanishes on average (Follow Pamela Silver Approach)
