673b604640278e182da7fffd9c041ad4dbf9df1a
chiffa <andrei.chiffa136@gmail.com>
2013-07-23 11:31:52 -0700

    Completed ad debugged

--- PolyPharma/neo4j_analyzer/knowledge_access.py
+++ PolyPharma/neo4j_analyzer/knowledge_access.py
@@ -14,6 +14,7 @@ from time import time
 import pickle
 import numpy as np
 import operator
+import Levenshtein as lv
 
 GOUpTypes=["is_a_go","is_part_of_go"]
 GORegTypes=["is_Regulant"]
@@ -143,10 +144,11 @@ def get_GO_Informativities():
             TimesReached[elt]+=1
     Fle=file('GO_Informativities.dump','w')
     pickle.dump(TimesReached,Fle)
+    Fle2=file('accDict.dump','w')
+    pickle.dump(accelerationDict,Fle2)
 
-def load_GO_informativities():
-    Fle = file('GO_Informativities.dump','r')
-    GO_Infos = pickle.load(Fle)
+def analyze_GO_Informativities():
+    GO_Infos = pickle.load(file('GO_Informativities.dump','r'))
     srted = sorted(GO_Infos.iteritems(), key=operator.itemgetter(1), reverse=True)
     GO_2_Names=pickle.load(file('GO_names.dump','r'))
     i=0
@@ -155,9 +157,21 @@ def load_GO_informativities():
         print i, key, val, GO_2_Names[key]
     return GO_Infos
 
-def load_GO_Structre():
+def load_GO_Informativities():
+    GO_Infos = pickle.load(file('GO_Informativities.dump','r'))
+    return GO_Infos
+
+def load_GO_Structure():
     GO_structure = pickle.load(file('GO_structure.dump','r'))
     return GO_structure
+
+def load_GO_Accesses():
+    GO_Accesses=pickle.load(file('GO.dump','r'))
+    return GO_Accesses
+
+def load_accDict():
+    accDict=pickle.load(file('accDict.dump','r'))
+    return accDict
 
 def acceleratedInsert(GO_structure,accelerationDict, name, InStack):
     GO_2_Names=pickle.load(file('GO_names.dump','r'))
@@ -178,6 +192,99 @@ def unzip(Supset, GO_2_Names):
         nameList.append(GO_2_Names[elt])
     return nameList
         
+def access_GO(UniprotSet):
+    GO_Accesses=load_GO_Accesses()
+    GO_Structure=load_GO_Structure()
+    GO_Informativities=load_GO_Informativities()
+    accDict=load_accDict()
+    EltDict={}
+    Count_Dict={}
+    for item in UniprotSet:
+        EltDict[item]=[]
+        for GO in GO_Accesses[item]:
+            InStack=[]
+            vs=acceleratedInsert(GO_Structure, accDict, GO, InStack)
+            EltDict[item]=EltDict[item]+vs
+        for GO in EltDict[item]:
+            if GO not in Count_Dict.keys():
+                Count_Dict[GO]=0
+            Count_Dict[GO]+=1
+    for elt in Count_Dict.key():
+        print Count_Dict[elt], GO_Informativities[elt]
+    return EltDict, Count_Dict
+
+def access_Reactional_Pathways(UniprotSet):
+    # Get the molecules from the reactome attached to the uniprot set
+    SP_ID2ReactNodes={}
+    for SP_ID in UniprotSet:
+        nodeGen=DatabaseGraph.UNIPORT.index.lookup(ID=SP_ID)
+        if nodeGen!=None:
+            SP_ID2ReactNodes=[]
+    # Get the reactional bindings incoming on those molecules or molecules they are directly related to ()
+
+def convert_SP_to_IDs(SP_List):
+    Res_Dict={}
+    for name in SP_List:
+        generator=DatabaseGraph.UNIPORT.index.lookup(ID=name)
+        if generator!=None:
+            Res_Dict[name]=[]
+            for elt in generator:
+                ID=str(elt).split('/')[-1][:-1]
+                Res_Dict[name].append(ID)
+            if Res_Dict[name]>1:
+                print 'Error: several references!', name, Res_Dict[name]
+            else: 
+                Res_Dict[name]=Res_Dict[name][0]
+    return Res_Dict
+
+
+def align_names2SP():
+    from Utils.UNIPROT_Parser import names_Dict
+    Fle=file('/home/andrei/workspaces/UCSD/NeflanavirSource.csv','r')
+    FileDict={}
+    i=0
+    print len(names_Dict)
+    while True:
+        i+=1
+        line=Fle.readline()
+        if not line:
+            break
+        if i>3:
+            words=line.split('\t')
+            FileDict[words[0]]=(words[1],words[2],words[3])
+    print len(FileDict)
+    LocalDict={}
+    Remainder=[]
+    # TODO: perform a symmetric string "find" over here
+    
+    for name in FileDict.keys():
+        if name.lower().strip() in names_Dict.keys():
+            LocalDict[name.lower().strip()]=names_Dict[name.lower().strip()]
+        else:
+            Remainder.append(name.lower().strip())
+    print 'Remainder', len(Remainder)
+    PosnameDict={}
+    for name in Remainder:
+        PosnameDict[name]={}
+        for possname in names_Dict.keys():
+            if lv.ratio(name,possname)>80:
+                PosnameDict[name][possname]=lv.ratio(name,possname)
+        if len(PosnameDict)<1:
+            del PosnameDict[name]
+        else:
+            print PosnameDict[name]
+    for name in PosnameDict.keys():
+        print name
+        srt=sorted(PosnameDict[name].iteritems(), key=operator.itemgetter(1))
+        for key, val in srt:
+            print '\t', key, '\t!|!\t', val
+ 
+ 
+
+       
+# TODO: Test on a GO_Terms set from the real Data   
+# TODO: add the modules for matrix operations over the GO annotation
+# TODO: add the propagation of the informativity along different GO Terms
     
 init=time()
 # filtr=['biological_process']
@@ -186,8 +293,7 @@ init=time()
 # get_GO_structure(filtr,SeedSet)
 # print 2, time()-init
 # get_GO_Informativities()
-load_GO_Structure()
-load_GO_informativities()
+align_names2SP()
 print 3, time()-init
 
 # TODO: check that each unipront points to a GO
