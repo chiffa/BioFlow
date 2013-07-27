21cdf53df49d3e66b518d5f1d6874b040a312ab4
chiffa <andrei.chiffa136@gmail.com>
2013-07-26 17:14:06 -0700

    debugged and Tested

--- PolyPharma/Utils/Load_Prot_Aboundances.py
+++ PolyPharma/Utils/Load_Prot_Aboundances.py
@@ -15,18 +15,25 @@ def compute_Prot_Aboundances():
     from configs import Prot_abound 
     SP2Aboundances={}
     Fle=file(Prot_abound,'r')
+    i=0
     while True:
         line=Fle.readline()
         if not line:
             break
+        print line
         if line[0]!='#':
-            words=line.split('\t')[1:2]
-            print words,words.split('.')[0],access_dict[words.split('.')[0]]
-            SP_ID=access_dict[words.split('.')[0]]
-            SP2Aboundances[SP_ID]=float(words[1])
-    Fle=file('SP2Aboundaces.dump','w')
-    pickle.dump(SP2Aboundances,Fle )
-    Fle.close()
+            words=line.split('\t')[1:3]
+            if  words[0].split('.')[1] in access_dict.keys():
+                i+=1
+                print words,
+                print words[0].split('.')[1],
+                print access_dict[words[0].split('.')[1]]
+                SP_ID=access_dict[words[0].split('.')[1]]
+                SP2Aboundances[SP_ID]=float(words[1])
+    FullFle=file('SP2Aboundaces.dump','w')
+    pickle.dump(SP2Aboundances,FullFle )
+    FullFle.close()
+    print i
     return SP2Aboundances
 
 def load_Prot_Aboundances():
@@ -36,7 +43,7 @@ def load_Prot_Aboundances():
     SP2Aboundances=pickle.load(file('SP2Aboundaces.dump','r'))
     return SP2Aboundances
 
-compute_Prot_Aboundances()
+# compute_Prot_Aboundances()
 SP2Aboundances=load_Prot_Aboundances()
 
 print len(SP2Aboundances)
