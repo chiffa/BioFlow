ea626ffe9f5b3ea344820e9ad4be5f9ace361f33
chiffa <andrei.chiffa136@gmail.com>
2013-08-15 02:39:11 -0700

    Largest part of the second piece written

--- PolyPharma/TODOList.py
+++ PolyPharma/TODOList.py
@@ -24,18 +24,6 @@ Created on Jun 25, 2013
 # TODO: for each uniprot gene iserted add a DNA reference to the gene that was actually concerned.
 # That sucks, because we will also need to insert all the names references and check for the possible collisions with the UNIPROT identifiers
 
-# TODO: Matrix retriever:
-# 1 -> 2 -> 3
-#
-# for each new insertion, look if it modifies any direct pathway
-# even better: make all the insertions at 1 on the diagonal, then sup triangularise the matrix and fill in the same way as for the energy interaction: take the shortest interaction
-# points (dynamic programming)
-#
-# Wait, can we do it by matrix multiplication? 
-#
-# 0 1 ?     0 1 2
-#   0 1  ->   0 1
-#     0         0
 
 # We are not taking in account the EC numbers of enzymes, since they do not point towards the reaction participants, just the catalysing elements.
 
