f9695f5f1bc56cf2cb441e5a80f1c7b04f6db495
chiffa <ank@stowers.org>
2013-12-07 19:37:10 -0600

    Initial remelting of the PolyPharma package

---
+++ PolyPharma/src/config.py
@@ -0,0 +1,23 @@
+'''
+Created on Dec 7, 2013
+
+@author: ank
+
+Allows to configure the execution nevironement
+'''
+
+'''
+Are the servers are on the same machine python code is running ?
+'''
+servers_are_local=True
+
+'''
+Where is the local copy of the neo4j database?
+'''
+local_neo4j="/usr/local/Cellar/neo4j/1.9.5/libexec/data"
+
+'''
+Where is the remote neo4j source?
+'''
+server_neo4j="http://localhost:7474"
+
