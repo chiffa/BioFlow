Corrected issues with Random Laplacian by permutation computation. Added visualization of random matrix off-term elements.

PolyPharma/Utils/Linalg_routines.py
@@ -4,8 +4,10 @@ from scipy.sparse import lil_matrix, triu
 from scipy.sparse.linalg import eigsh
 import numpy as np
 from copy import copy
-from itertools import repeat
-from random import randrange
+from itertools import izip
+from random import randrange, shuffle
+from time import time
+import matplotlib.pyplot as plt
 
 def Lapl_normalize(non_normalized_Laplacian):
     """
@@ -16,14 +18,44 @@ def Lapl_normalize(non_normalized_Laplacian):
     :return:
     """
     normalized_Laplacian = lil_matrix(copy(non_normalized_Laplacian))
-    diagterms = normalized_Laplacian.diagonal().tolist()
-    for i, term in enumerate(diagterms):
-        normalized_Laplacian[i, :] = normalized_Laplacian[i, :]/np.sqrt(term)
-        normalized_Laplacian[:, i] = normalized_Laplacian[:, i]/np.sqrt(term)
-    return normalized_Laplacian
+    diagterms = np.array(normalized_Laplacian.diagonal().tolist())
+    diagterms[diagterms == 0] = 1
+    diagterms = np.reshape(diagterms,(normalized_Laplacian.shape[0],1))
+    normmat = np.sqrt(np.dot(diagterms, diagterms.T))
+    retmat = lil_matrix(normalized_Laplacian/normmat)
+    # retmat[retmat > 1.0] = 1.0
+    # retmat[retmat <- 1.0] = -1.0
+    return retmat
 
-def show_eigenvals_and_eigenvects(eigvals, eigvects, biggest_limit, annotation):
-    print annotation, eigvals
+
+def show_eigenvals_and_eigenvects(eigvals, eigvects, biggest_limit, annotation, idx_chars=None):
+    print annotation
+    print '########################'
+    if not idx_chars:
+        for eigval in eigvals.tolist():
+            print eigval
+        print '<============================>\n'
+        return None
+
+    absolute = np.absolute(eigvects)
+    eigbiggest = np.argsort(absolute, axis=0)[-biggest_limit:, :]
+    setmemory = []
+    for i, eigval in enumerate(reversed(eigvals.tolist())):
+        renderstring = []
+        renderstring.append('associated eigval:'+str(eigval))
+        renderstring.append('Index \t value \t Descriptor')
+        mset = set()
+        for index, value in reversed(zip(eigbiggest[:, i], absolute[eigbiggest[:, i], i])):
+            if int(value**2*1000) > 0:
+                mset.add(index)
+                renderstring.append('\t'+str(index)+'\t'+str('{0:.4f}'.format(value**2))+'\t'+str(idx_chars[index]))
+        renderstring.append('<==================>')
+        if mset not in setmemory:
+            setmemory.append(mset)
+            print '\n'.join(renderstring)
+
+    return None
+
     # absolute_vals = np.absolute(eigvals)
     # biggest_vals = np.argsort(absolute_vals, axis=0)[-biggest_limit:, :]
     # print biggest_vals
@@ -37,46 +69,72 @@ def show_eigenvals_and_eigenvects(eigvals, eigvects, biggest_limit, annotation):
     #     Retname.add(str(MG.get_descriptor_for_index(arg)[1]))
 
 
-def analyze_eigvects(non_normalized_Laplacian, num_first_eigvals_to_analyse):
-
+def analyze_eigvects(non_normalized_Laplacian, num_first_eigvals_to_analyse, index_chars, permutations_limiter = 10000000, fudge=10e-10):
     # normalize the laplacian
+    print 'analyzing the laplacian with %s items and %s non-zero elts' % (non_normalized_Laplacian.shape[0]**2, len(non_normalized_Laplacian.nonzero()[0]))
+    t = time()
+    init = time()
     normalized_Laplacian = Lapl_normalize(non_normalized_Laplacian)
+    print time()-t
+    t = time()
     # compute the eigenvalues and storre them
     true_eigenvals, true_eigenvects = eigsh(normalized_Laplacian, num_first_eigvals_to_analyse)
+    print time()-t
+    t = time()
     # permute randomly the off-diagonal terms
-    triag_u = lil_matrix(triu(non_normalized_Laplacian))
+    triag_u = lil_matrix(triu(normalized_Laplacian))
     triag_u.setdiag(0)
-    for _ in repeat(None, non_normalized_Laplacian.shape[0]**2/2):
-        i = randrange(1, non_normalized_Laplacian.shape[0]-1)
-        j = randrange(i+1, non_normalized_Laplacian.shape[0])
-        triag_u[i,j], triag_u[j,i] = (triag_u[j,i], triag_u[i,j])
-
+    tnz = triag_u.nonzero()
+    print "reassigning the indexes for %s items, with %s non-zero elts" % (triag_u.shape[0]**2, len(tnz[0]))
+    eltsuite = zip(tnz[0].tolist(), tnz[1].tolist())
+    shuffle(eltsuite)
+    if eltsuite > permutations_limiter:
+        eltsuite = eltsuite[:permutations_limiter]   # pb: we want it to affect any random number with reinsertion
+    print time()-t
+    t = time()
+    # take a nonzero pair of indexes
+    for i,j in eltsuite:
+        # select randomly a pair of indexes and permute it
+        k = randrange(1, triag_u.shape[0]-1)
+        l = randrange(k+1, triag_u.shape[0])
+        triag_u[i, j], triag_u[k,l] = (triag_u[k, l], triag_u[i, j])
+    print time()-t
+    t = time()
     # recompute the diagonal terms
     fullmat = triag_u + triag_u.T
-    diagterms = [item for sublist in fullmat.sum(axis=0).tolist() for item in sublist]
+    diagterms = [-item for sublist in fullmat.sum(axis=0).tolist() for item in sublist]
     fullmat.setdiag(diagterms)
+    print time()-t
+    t = time()
     # recompute the normalized matrix
     normalized_rand = Lapl_normalize(fullmat)
     # recompute the eigenvalues
     rand_eigenvals, rand_eigenvects = eigsh(normalized_rand, num_first_eigvals_to_analyse)
+    print time()-t
+    t = time()
+    show_eigenvals_and_eigenvects(true_eigenvals, true_eigenvects, 20, 'true laplacian', index_chars)
+    show_eigenvals_and_eigenvects(rand_eigenvals, rand_eigenvects, 20, 'random')
+    print "final", time()-t, time()-init
 
-    show_eigenvals_and_eigenvects(true_eigenvals, true_eigenvects, 10, 'true')
-    show_eigenvals_and_eigenvects(rand_eigenvals, rand_eigenvects, 10, 'random')
 
-    # normation and return of associated terms
-
-
+def view_laplacian_off_terms(non_normalized_Laplacian):
+    normalized_Laplacian = Lapl_normalize(non_normalized_Laplacian)
+    triag_u = lil_matrix(triu(normalized_Laplacian))
+    triag_u.setdiag(0)
+    pre_arr = -triag_u[triag_u.nonzero()].toarray().flatten()
+    arr = np.log10(pre_arr)
+    plt.hist(arr, bins=100, log=True, histtype='step')
+    plt.show()
 
 
 if __name__ == "__main__":
-    test_lapl = lil_matrix(np.zeros((3, 3)))
-    test_lapl.setdiag([1, 2, 3])
-    test_lapl[1, 2] = 2
-    test_lapl[2, 1] = 2
-    test_lapl[0, 2] = 1
-    test_lapl[2, 0] = 1
+    test_lapl = lil_matrix(np.zeros((4, 4)))
+    test_lapl.setdiag([1, 2, 3, 0])
+    test_lapl[1, 2] = -2
+    test_lapl[2, 1] = -2
+    test_lapl[0, 2] = -1
+    test_lapl[2, 0] = -1
     print test_lapl.toarray()
     print Lapl_normalize(test_lapl).toarray()
 
-
-    analyze_eigvects(test_lapl,2)
+    analyze_eigvects(test_lapl, 2, {0:'zero', 1:'one', 2:'two', 3: 'three'})

PolyPharma/neo4j_analyzer/knowledge_access_analysis.py
@@ -13,13 +13,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 from copy import copy
 from random import shuffle
-from PolyPharma.Utils.better_histogram import better2D_desisty_plot
+from PolyPharma.Utils.dataviz import better2D_desisty_plot
 from itertools import combinations
 from scipy.sparse import lil_matrix
 from PolyPharma.Utils.Linalg_routines import analyze_eigvects
 
 filtr = ['biological_process']
-corrfactors = (0.5, 2)
+corrfactors = (0.3, 2)
 pprinter = PrettyPrinter(indent=4)
 MG = MatrixGetter(True, False)
 MG.fast_load()
@@ -117,6 +117,7 @@ def stats_on_existing_circsys(size, slector):
 
     :return:
     """
+    # TODO: build a comparator of actual data distribution with expected data distribution
 
     KG = KG_gen()
     MD5_hash = KG._MD5hash()
@@ -187,17 +188,19 @@ def get_estimated_time(samples, sample_sizes, operations_per_sec=2.2):
     return counter
 
 
-def linindep_GO_groups():
+def linindep_GO_groups(size):
     KG = KG_gen()
     KG.undump_Indep_Linset()
-    print KG._time()
-    analyze_eigvects(KG.Indep_Lapl, 50)
-
+    char_indexes = dict( (key, (len(KG.GO2UP_Reachable_nodes[value]), KG.GO_Legacy_IDs[value], KG.GO_Names[value])) for key, value in KG.Num2GO.iteritems())
+    print KG.pretty_time()
+    analyze_eigvects(KG.Indep_Lapl, size, char_indexes)
 
 
 if __name__ == "__main__":
     # spawn_sampler(([10, 100], [2, 1]))
-    # spawn_sampler_pool(4, [10, 25, 50, 100], [15, 10, 10, 8])
+    # spawn_sampler_pool(4, [100], [8])
     # get_estimated_time([10, 25, 50, 100,], [15, 10, 10, 8,])
-    # stats_on_existing_circsys(10, [1000, 1200])
-    linindep_GO_groups()
+    # stats_on_existing_circsys(100, [1000, 1200])
+    linindep_GO_groups(50)
+
+    pass