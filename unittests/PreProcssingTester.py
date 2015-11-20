9bee2722c3dadeb5cf75be01de583e0d14ca252c
chiffa <andrei.chiffa136@gmail.com>
2015-11-20 08:53:13 -0500

    Pulled back the  proper testing in travis and excluded unittests from QuantifiedCodeChecks

---
+++ unittests/PreProcssingTester.py
@@ -0,0 +1,23 @@
+"""
+Tests the preprocessing module
+"""
+
+import unittest
+from BioFlow.PreProcessing import RnaCountsProcessor
+
+
+class TestRnaCountsProcessor(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_table_loading(self):
+        pass
+
+if __name__ == "__main__":
+    unittest.main()
