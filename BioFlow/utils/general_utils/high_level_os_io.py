fedc41a07184da3136c4d3ccc108d66f9ecc04a6
chiffa <andrei.chiffa136@gmail.com>
2015-12-14 16:51:33 -0500

    corrected logging behavior

--- BioFlow/utils/general_utils/high_level_os_io.py
+++ BioFlow/utils/general_utils/high_level_os_io.py
@@ -14,17 +14,17 @@ def mkdir_recursive(path):  # pragma: no cover
     :return:
     """
     logger.debug(
-        'trying to create recursively path containing: {0}'.format(path))
+        'trying to create recursively path containing: %s', path)
     path = os.path.abspath(path)
     directory_name = os.path.dirname(path)
-    logger.debug('subpath: {0}'.format(directory_name))
+    logger.debug('subpath: %s', directory_name)
     if not os.path.exists(directory_name):
         mkdir_recursive(directory_name)
     if not os.path.exists(path):
-        logger.debug('path {0} does not exist yet, looks like file: {1}'.format(
-            path, '.' in path.split('/')[-1][-5:]))
-        if '.' not in path.split(
-                '/')[-1][-5:]:  # should be able to suppress specific file creation
+        logger.debug('path %s does not exist yet, looks like file: %s' %
+                     (path, '.' in path.split('/')[-1][-5:]))
+        if '.' not in path.split('/')[-1][-5:]:
+            # should be able to suppress specific file creation
             os.mkdir(path)
             logger.debug('; created')
         else:
@@ -49,16 +49,16 @@ def wipe_dir(path):  # pragma: no cover
     else:
         directory_name = os.path.dirname(path)
 
-    logger.debug('going to wipe {0} for path {1}'.format(directory_name, path))
+    logger.debug('going to wipe %s for path %s', directory_name, path)
     if not os.path.isdir(directory_name):
         logger.exception(
-            'failed to delete {0}: for path {1}, not a dir'.format(directory_name, path))
+            'failed to delete %s: for path %s, not a dir', directory_name, path)
         return False
 
     for sub_path in os.listdir(directory_name):
         if os.path.isdir(sub_path):
             logger.exception(
-                'failed to delete {0}: for path {1}, anti rm -rf flag'.format(directory_name, path))
+                'failed to delete %s: for path %s, anti rm -rf flag', directory_name, path)
             return False
 
     else:
