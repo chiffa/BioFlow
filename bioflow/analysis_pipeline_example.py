db5f3a84843fe460968ac72d3837e6529c348b4a
chiffa <andrei.chiffa136@gmail.com>
2021-08-25 15:51:49 +0200

    added validation sources

--- bioflow/analysis_pipeline_example.py
+++ bioflow/analysis_pipeline_example.py
@@ -141,58 +141,95 @@ if __name__ == "__main__":
     #      'yeast_test_gene_set-glycogen_biosynthesis_tsw_2.tsv'),
     #      'yeast_test_weighted_background.tsv')
 
-    # hits_ids, sec_hit_ids, background_internal_ids = map_and_save_gene_ids(
-    #      'yeast_test_gene_set-glycogen_biosynthesis.tsv',
-    #      '')
-    #
+    test_folder_root = '/localhome/kucharav/Ewald Lab validation/distilled/'
+
+    matrigel_collagen_tumor = 'Collagen-Matrigel/Matrigel_vs_collagen-tumor.tsv'
+    matrigel_collagen_normal = 'Collagen-Matrigel/Matrigel_vs_collagen-normal.tsv'
+
+    kp_km_mouse = 'Kp-Km/top_100.csv'
+    kp_km_human = 'Kp-Km/top_100_hum.csv'
+    kp_km_20_rd_drop = 'Kp-Km/top_80 - 20p drop-out.csv'
+
+    K14_base = 'K14/All_genes.csv'
+    K14_human = 'K14/both_HUM.csv'
+    K14_mouse = 'K14/both.csv'
+
+    TWIST_1_base = 'TWIST-1/All_genes.csv'
+    TWIST_1_hits = 'TWIST-1/Hits.csv'
+
+
+    second_folder_root = '/localhome/kucharav/bioflow validation data'
+
+    breast_cancer_1q = 'breast cancer aneuploidy/1q.txt'
+    breast_cancer_8p = 'breast cancer aneuploidy/8p.txt'
+    breast_cancer_8q = 'breast cancer aneuploidy/8q.txt'
+    breast_cancer_8q2 = 'breast cancer aneuploidy/8q - second half.txt'
+    breast_cancer_18q = 'breast cancer aneuploidy/18q.txt'
+    breast_cancer_20q = 'breast cancer aneuploidy/20q.txt'
+
+    Linhao_no_import_hits = '"yeast -  Linhao no import screens"/hits.csv'
+    Linhao_no_import_background = '"yeast -  Linhao no import screens"background.csv'
+
+    Linhao_MudPIT_hits = 'yeast - Linhao MudPIT/background.csv'
+    Linhao_MudPIT_background = 'yeast - Linhao MudPIT/hits_WT-MUT_vs_CTRL.csv'
+
+
+    active_load = os.path.join(second_folder_root, breast_cancer_20q)
+    active_back = ''
+    # active_back = os.path.join(second_folder_root, TWIST_1_base)
+
+    hits_ids, sec_hit_ids, background_internal_ids = map_and_save_gene_ids(
+         active_load, active_back)
+
     # log.debug('debug: hits_ids parse: %s' % hits_ids)
-    #
-    # interactome_analysis(source_list=hits_ids,
-    #                      secondary_source_list=sec_hit_ids,
-    #                      # output_destinations_list=['chr_%s' % filename[:-4]],
-    #                      random_samples_to_test_against=5,
-    #                      processors=1,
-    #                      background_list=background_internal_ids,
-    #                      skip_sampling=False,
-    #                      forced_lapl_reweight={155606: 0.0}
-    #                      )
 
-    # knowledge_analysis(source_list=hits_ids,
-    #                    secondary_source_list=sec_hit_ids,
-    #                    # output_destinations_list=['chr_%s' % filename[:-4]],
-    #                    random_samples_to_test_against=5,
-    #                    processors=1,
-    #                    background_list=background_internal_ids,
-    #                    skip_sampling=False,
-    #                    )
-
-    chromosomes_directory = "//localhome//kucharav//Projects//BioFlow paper//yeast_chr_genes"
-    background_file = os.path.join(chromosomes_directory, "all_genes.tab")
-
-    chromosomes_files = []
-    name_files = []
-    for filename in os.listdir(chromosomes_directory):
-        if filename != "all_genes.tab":
-            chromosomes_files.append(os.path.join(chromosomes_directory, filename))
-            name_files.append('chr_%s' % filename[:-4])
-
-    hits_ids, sec_hit_ids, background_ids = map_and_save_gene_ids(chromosomes_files, background_file)
+    log.info('Registration: run from %s' % active_load)
 
     interactome_analysis(source_list=hits_ids,
-                         output_destinations_list=name_files,
+                         secondary_source_list=sec_hit_ids,
+                         output_destinations_list=['BR_C_8q'],
                          random_samples_to_test_against=25,
                          processors=1,
-                         background_list=background_ids,
+                         background_list=background_internal_ids,
                          skip_sampling=False
                          )
 
     knowledge_analysis(source_list=hits_ids,
-                       output_destinations_list=name_files,
+                       secondary_source_list=sec_hit_ids,
+                       output_destinations_list=['BR_C_8q'],
                        random_samples_to_test_against=25,
                        processors=1,
-                       background_list=background_ids,
+                       background_list=background_internal_ids,
                        skip_sampling=False,
                        )
+
+    # chromosomes_directory = "//localhome//kucharav//Projects//BioFlow paper//yeast_chr_genes"
+    # background_file = os.path.join(chromosomes_directory, "all_genes.tab")
+    #
+    # chromosomes_files = []
+    # name_files = []
+    # for filename in os.listdir(chromosomes_directory):
+    #     if filename != "all_genes.tab":
+    #         chromosomes_files.append(os.path.join(chromosomes_directory, filename))
+    #         name_files.append('chr_%s' % filename[:-4])
+    #
+    # hits_ids, sec_hit_ids, background_ids = map_and_save_gene_ids(chromosomes_files, background_file)
+    #
+    # interactome_analysis(source_list=hits_ids,
+    #                      output_destinations_list=name_files,
+    #                      random_samples_to_test_against=25,
+    #                      processors=1,
+    #                      background_list=background_ids,
+    #                      skip_sampling=False
+    #                      )
+    #
+    # knowledge_analysis(source_list=hits_ids,
+    #                    output_destinations_list=name_files,
+    #                    random_samples_to_test_against=25,
+    #                    processors=1,
+    #                    background_list=background_ids,
+    #                    skip_sampling=False,
+    #                    )
 
 
     # # compile chromosome file names
